e67662c4d5cb9fc166015486cb5d8862e752c8d9
celuk <seyyid4091@gmail.com>
2026-01-16 14:57:23 +0300

    get back to the old one

--- riscv-busybox-port/compile.sh
+++ riscv-busybox-port/compile.sh
@@ -4,7 +4,7 @@ sudo rm -rf ./_install ./rootfs.cpio.gz;
 make distclean;
 make defconfig;
 sed -i 's/# CONFIG_STATIC is not set/CONFIG_STATIC=y/' .config;
-export CROSS_COMPILE=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install2/bin/riscv64-unknown-linux-gnu-;
+export CROSS_COMPILE=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-;
 make -j14;
 make install;
 
--- riscv-linux-port/compile.sh
+++ riscv-linux-port/compile.sh
@@ -3,12 +3,12 @@ set -e
 make clean;
 make mrproper;
 make distclean;
-make ARCH=riscv CROSS_COMPILE=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install2/bin/riscv64-unknown-linux-gnu- 64-bit.config;
-make ARCH=riscv CROSS_COMPILE=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install2/bin/riscv64-unknown-linux-gnu- -j16;
-/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install2/bin/riscv64-unknown-linux-gnu-objdump -m riscv:rv64 -d vmlinux > vmlinux.dump;
+make ARCH=riscv CROSS_COMPILE=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu- 64-bit.config;
+make ARCH=riscv CROSS_COMPILE=/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu- -j16;
+/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-objdump -m riscv:rv64 -d vmlinux > vmlinux.dump;
 ## Image = vmlinux.bin
 #dd if=/dev/zero bs=1 count=4096 >> ./arch/riscv/boot/Image
 python3 /home/shc/projects/cva-soc/tools/bin2hex.py ./arch/riscv/boot/Image > ./arch/riscv/boot/Image.hex;
-#/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install2/bin/riscv64-unknown-linux-gnu-objcopy -O binary ./vmlinux vmlinux.bin
+#/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-objcopy -O binary ./vmlinux vmlinux.bin
 #python3 /home/shc/projects/cva-soc/tools/bin2hex.py ./vmlinux.bin > ./vmlinux.hex;
-/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install2/bin/riscv64-unknown-linux-gnu-objcopy -O verilog ./vmlinux ./vmlinux.vmem;
+/media/shc/0EDEBC4906059163/tools/riscv-toolchain-linux/_install/bin/riscv64-unknown-linux-gnu-objcopy -O verilog ./vmlinux ./vmlinux.vmem;
