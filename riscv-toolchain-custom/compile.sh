76ff72d49fe7462ea7fcbb5a68fb73d04166c36f
celuk <seyyid4091@gmail.com>
2026-03-18 16:02:38 +0300

    try 10mhz

--- riscv-toolchain-custom/compile.sh
+++ riscv-toolchain-custom/compile.sh
@@ -2,6 +2,6 @@ set -e
 
 pushd riscv-gnu-toolchain
 make distclean
-./configure --prefix=$(pwd)/../_install
+./configure --prefix=$(pwd)/../_install --with-arch=rv64imac --with-abi=lp64
 make linux -j16
 popd
